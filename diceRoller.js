40abaea7a24efd1f60dcced00caaf315eb0da33a
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-02-11 12:35:27 -0500

    Eureka! It finally works! Had to take the function out :)

--- diceRoller.js
+++ diceRoller.js
@@ -5,8 +5,15 @@
 var sides = [1, 2, 3, 4, 5, 6]
 // Defines the different sides of the dice
 
-var rollDice = function() {
-	return Math.floor(Math.random(sides) * sides.length);
-// Roll a single dice
-	}
+
+
+
+var rollDice = sides[Math.floor(Math.random() * sides.length)];
+// Assigns a random number to the dice
+
+
+
+
+
 console.log(rollDice);
+// Prints out the assigned number, simulating a dice roll
