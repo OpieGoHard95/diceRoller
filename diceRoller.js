ae8da4e617dc82ef98ee9a145aed64c9f0b7d029
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-02-11 12:07:05 -0500

    First attempt at the problem, recieved multiple error codes

---
+++ diceRoller.js
@@ -0,0 +1,9 @@
+// Coded by Jared Smith
+// Started Feb 9 ended Feb 11
+// Dice Rolling problem that was assigned in class
+
+var rollDice = function {
+	return Math.floor(Math.random() * 6);
+// Roll a single dice
+}
+console.log(rollDice);
